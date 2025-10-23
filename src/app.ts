1ce0e97498a402c91a94cff461c276b458f1d2a1
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-22 22:26:17 -0600

    Set up Express server and configuration for environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import express from "express";
+
+import { EXPRESS_PORT } from "@/config.js";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Server is running on http://localhost:${EXPRESS_PORT}`);
+});
